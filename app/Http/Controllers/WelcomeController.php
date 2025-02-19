0e2bebc5dc7477dbc2f7231423ed2de0b327934b
NaafiRidho <naafiridho0505@gmail.com>
2025-02-19 16:02:39 +0700

    membuat kontroller

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function hello(){
+        return 'Hello World';
+    }
+
+    public function index(){
+        return('Selamat Datang');
+    }
+
+    public function about(){
+        return ('Naafi Ridho A/22');
+    }
+
+    public function articles($id){
+        return 'Artikel ke- '.$id;
+    }
+}
